058146458ad7b6cb30a0225547202269b8a692ae
qianyu chen <38046403+qyc-98@users.noreply.github.com>
2022-10-19 10:01:32 +0800

    Update grounding_dataset.py

--- dataset/grounding_dataset.py
+++ dataset/grounding_dataset.py
@@ -1,3 +1,9 @@
+# This file is code for making dataset for visual grounding tasks.
+# Author: Qianyu Chen
+# Date: 2022-10
+ 
+# Copyright (c) THUNLP, Tsinghua University. All rights reserved. 
+# See LICENSE file in the project root for license information.
 import os
 import re
 import PIL
